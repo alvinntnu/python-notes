7f33c6c8dd621db963ef23bea29fde2b97fbfbf7
alvinntnu <alvinworks@gmail.com>
2020-09-12 11:46:32 +0800

    all

---
+++ _build/jupyter_execute/docs/jupyter_execute/temp-notes.py
@@ -0,0 +1,18 @@
+## Python Fundamentals
+
+- Basic structure
+- Data structure
+- Classes and Objects
+
+## Citation
+
+- In-text citation
+
+An example of in-text citation {cite}`deyne2016`.
+
+- Bibliography
+
+```{bibliography} book.bib
+:style: unsrt
+```
+
