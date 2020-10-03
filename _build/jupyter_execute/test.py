7b51bd0efeeb6599d2e49afc1e7d6ff86f7632c0
alvinntnu <alvinworks@gmail.com>
2020-10-03 21:47:34 +0800

    gdriveversion

---
+++ _build/jupyter_execute/test.py
@@ -0,0 +1,17 @@
+# Test Colab Notebook
+
+# Test Colab Notebook
+
+- Updates from local
+- See if i can push the changes onto github
+- and pull the changes from colab
+
+import os
+
+print(os.getcwd())
+
+print('I modified the code on Google Colab.')
+
+1. Test 1
+2. Test 2
+3. Test 3
