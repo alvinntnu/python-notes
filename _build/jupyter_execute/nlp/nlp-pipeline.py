552c507e2c40e5475e22933406d479eabdde7fd4
alvinntnu <alvinworks@gmail.com>
2021-01-14 03:22:27 +0800

    nlp

---
+++ _build/jupyter_execute/nlp/nlp-pipeline.py
@@ -0,0 +1,13 @@
+
+# Natural Language Processing Pipeline
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+nlp-spacy
+nlp-spacy-zh
+nlp-ckipnlp
+```
---
+++ _build/jupyter_execute/nlp/text-normalization-intro.py
@@ -0,0 +1,11 @@
+# Text Normalization
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+text-normalization-eng
+text-normalization-chinese
+```
