f5a0f8fa7917ca02e39d3738d9938ddeb887751a
alvinntnu <alvinworks@gmail.com>
2021-01-18 11:33:52 +0800

    nlp-pipeline

--- _build/jupyter_execute/nlp/nlp-pipeline.py
+++ _build/jupyter_execute/nlp/nlp-pipeline.py
@@ -1,13 +1,225 @@
+# NLP Pipeline
 
-# Natural Language Processing Pipeline
+## A General NLP Pipeline
+
+![nlp-pipeline](../images/nlp-pipeline.png)
+
+### Varations of the NLP Pipelines
+
+- The process may not always be linear.
+- There are loops in between.
+- These procedures may depend on specific task at hand.
+
+## Data Collection
+
+### Data Acquisition: Heart of ML System
+
+- Ideal Setting: We have everything needed.
+- Labels and Annotations
+- Very often we are dealing with less-than-idea scenarios
+
+### Less-than-ideal Scenarios
+
+- Initial datasets with limited annotations/labels
+- Initial datasets labeled based on regular expressions or heuristics
+- Public datasets (cf. [Google Dataset Search](https://datasetsearch.research.google.com/))
+- Scrape data
+- Product intervention
+- Data augmentation
+
+### Data Augmentation
+
+- It is a technique to exploit language properties to create texts that are syntactically similar to the source text data.
+- Types of strategies:
+    - synonym replacement
+    - Related word replacement (based on association metrics)
+    - Back translation
+    - Replacing entities
+    - Adding noise to data (e.g. spelling errors, random words)
+
+## Text Extraction and Cleanup
+
+### Text Extraction
+
+- Extracting raw texts from the input data
+    - HTML
+    - PDF
+- Relevant vs. irrelevant information
+    - non-textual information
+    - markup
+    - metadata
+- Encoding format
+
+#### Extracting texts from webpages
+
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+ 
+ 
+url = 'https://news.google.com/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNRFptTXpJU0JYcG9MVlJYS0FBUAE?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
+r = requests.get(url)
+web_content = r.text
+soup = BeautifulSoup(web_content,'html.parser')
+title = soup.find_all('a', class_='DY5T1d')
+first_art_link = title[0]['href'].replace('.','https://news.google.com',1)
+
+#print(first_art_link)
+art_request = requests.get(first_art_link)
+art_request.encoding='utf8'
+soup_art = BeautifulSoup(art_request.text,'html.parser')
+
+art_content = soup_art.find_all('p')
+art_texts = [p.text for p in art_content]
+print(art_texts)
 
 
-```{toctree}
-:hidden:
-:titlesonly:
+#### Extracting texts from scanned PDF
+
+from PIL import Image
+from pytesseract import image_to_string
+
+filename = '../../../RepositoryData/data/pdf-firth-text.png'
+text = image_to_string(Image.open(filename))
+print(text)
+
+#### Unicode normalization
+
+text = 'I feel really 😡. GOGOGO!! 💪💪💪  🤣🤣'
+print(text)
+text2 = text.encode('utf-8')
+print(text2)
 
 
-nlp-spacy
-nlp-spacy-zh
-nlp-ckipnlp
-```
+- Other useful libraries
+    - Spelling check: pyenchant, Microsoft REST API
+    - PDF:  PyPDF, PDFMiner
+    - OCR: pytesseract
+ 
+
+### Cleanup
+
+- Preliminaries
+    - Sentence segmentation
+    - Word tokenization
+    
+
+#### Segmentation and Tokenization
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+text = '''
+Python is an interpreted, high-level and general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.
+'''
+
+## sent segmentation
+sents = sent_tokenize(text)
+
+## word tokenization
+for sent in sents:
+    print(sent)
+    print(word_tokenize(sent))
+
+- Frequent preprocessing
+    - Stopword removal
+    - Stemming and/or lemmatization
+    - Digits/Punctuaions removal
+    - Case normalization
+    
+
+#### Removing stopwords, punctuations, digits
+
+from nltk.corpus import stopwords
+from string import punctuation
+
+eng_stopwords = stopwords.words('english')
+
+text = "Mr. John O'Neil works at Wonderland, located at 245 Goleta Avenue, CA., 74208."
+
+words = word_tokenize(text)
+
+print(words)
+
+# remove stopwords, punctuations, digits
+for w in words:
+    if w not in eng_stopwords and w not in punctuation and not w.isdigit():
+        print(w)
+
+#### Stemming and lemmatization
+
+## Stemming
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+words = ['cars','revolution', 'better']
+print([stemmer.stem(w) for w in words])
+
+
+## Lemmatization
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+## Wordnet requires POS of words
+poss = ['n','n','a']
+
+for w,p in zip(words,poss):
+    print(lemmatizer.lemmatize(w, pos=p))
+
+- Task-specific preprocessing
+    - Unicode normalization
+    - language detection
+    - code mixing
+    - transliteration
+    
+
+- Automatic annotations
+    - POS tagging
+    - Parsing
+    - Named entity recognition
+    - coreference resolution
+    
+
+### Important Reminders for Preprocessing
+
+- Not all steps are necessary
+- These steps are NOT sequential
+- These steps are task-dependent
+
+## Feature Engineering
+
+### What is feature engineering?
+
+- It refers to a process to feed the extracted and preprocessed texts into a machine-learning algorithm.
+- It aims at capturing the characteristics of the text into a numeric vector that can be understood by the ML algorithms. (Cf. *construct*, *operational definitions*, and *measurement* in experimental science)
+- In short, it concerns how to meaningfully represent texts quantitatively, i.e., text representation.
+
+### Feature Engineering for Classical ML
+
+- word-based frequency lists
+- bag-of-words representations
+- domain-specific word frequency lists
+- handcrafted features based on domain-specific knowledge
+
+### Feature Engineering for DL
+
+- DL directly takes the texts as inputs to the model.
+- The DL model is capable of learning features from the texts (e.g., embeddings)
+- Less interpretable.
+    
+
+## Modeling
+
+### From Simple to Complex
+
+- Start with heuristics or rules
+- Experiment with different ML models
+    - from heuristics to features
+    - from manual annotation to automatic extraction
+    - feature importance (weights)
+- Find the most optimal model
+    - Ensemble and stacking
+    - Redo feature engineering
+    - Transfer learning
+    - Reapply heuristics
+
+## Evaluation
---
+++ _build/jupyter_execute/nlp/nlp-primer.py
@@ -0,0 +1,114 @@
+
+# Natural Language Processing: A Primer
+
+- Computational Linguistics is a very active subject in linguistics.
+- Natural Language Processing (NLP): It is a sub-field of computer science that deals with methods to analyze, model, and understand human language.
+
+
+## NLP Applications
+
+![nlp-apps](../images/nlp-apps.png)
+
+## NLP Tasks and Language Blocks
+
+![lg-blocks](../images/lg-blocks.png)
+
+## NLP Challenges
+
+- Ambiguity
+- Creativity
+- Diversity
+- Common Knowledge (Context)
+
+## Artificial Intelligence, Machine Learning, Deep Learning and NLP
+
+- **Artificial Intelligence** is a branch of computer sceience that aims to build systems that can perform taskes that require human intelligence.
+- **Machine Learning** is a branch of AI that deals with the development of algorithms that can learn to perform tasks automatically based on large number of examples, without requiring handcrafted rules.
+- **Deep Learning** is a branch of machine learning that is based on the artificial neural network architectures. 
+
+- Approaches to NLP
+    - Heuristics-based NLP
+    - Machine Learning NLP
+    - Deep Learning for NLP
+
+### Heuristics-based NLP
+
+Examples:
+   - Dictionary-based sentiment analysis
+   - WordNet for lexical relations
+   - Common sense world knowledge (Open Mind Common Sense, Ontoloty)
+   - Regular Expressions
+   - Context-free grammar
+
+Strengths:
+   - Rules based on domain-specific knowledge can efficiently reduce the mistakes that are sometimes very expensive.
+    
+
+### Machine Learning for NLP
+
+- Types of machine learning:
+    - Supervised vs. Unsupervised
+    - Classification vs. Regression
+
+- Three common steps for machine learning
+    - extracting features from texts
+    - using the feature representation to learn a model
+    - evaluating and improving the model
+
+- Common methods:
+   - Naive Bayes
+   - Support Vector Machine
+   - Hidden Markov Model
+   - Conditional Random Field
+
+### Deep Learning for NLP
+
+- Sequence Models
+    - Recurrent Neural Network (RNN)
+    - Long-Term Short-Term Memory (LSTM)
+- Convolutional Neural Network (CNN)
+
+- Transformers
+    - The state-of-the-art model in major NLP tasks
+    - It models the textual context in a non-sequential mannwer.
+    - Given a word in the input, the model looks at all the words around it and represent each word with respect to its context. This is referred to as *self-attention*. 
+
+- Transfer Learning
+    - It is a technique in AI where the knowledge gained while solving one problem is applied to a different but related problem.
+    - We can use unsupervised methods to train a transformer-based model for predicting a part of a sentence given the rest of the content. 
+    - This model can encode high-level nuances of the language, which can be applied to other relevant downstream tasks.
+
+## Deep Learning is NOT Everything
+
+- The trend now is to leverage large transformer models and huge datasets for generic NLP tasks like language models, and then adapt these pre-trained models to smaller downstream tasks. 
+
+- Challenges
+    - Overfitting on small datasets
+        - Occam's razor
+        - Data accessibility
+    - Domain adaptation
+    - Interpretable models
+    - Cost
+        - Data guzzler
+        - Specialized hardware (GPUs)
+        - Deployment and maintenance
+    - On-device deployment
+        
+
+## Conclusion
+
+- Deep learning is not always the go-to solution for all industrial NLP applications.
+
+- Many deep learning models are not interpretable enough to indicate the sources of empirical gains.
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+nlp-pipeline
+nlp-spacy
+nlp-spacy-zh
+nlp-ckipnlp
+```
