0113c6863e247709fbb727237a2ca346c61d6915
alvinntnu <alvinworks@gmail.com>
2021-03-24 13:03:16 +0800

    all

--- _build/jupyter_execute/python-basics/python-tricks.py
+++ _build/jupyter_execute/python-basics/python-tricks.py
@@ -190,3 +190,76 @@ def transpose_list(list_of_lists):
 
 transpose_list([[1, 4, 7], [2, 5, 8], [3, 6, 9]])
 [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+## Find Index of Specific Values in List
+
+Four methods to identify indices of List element which are specific values.
+
+# Python3 code to demonstrate  
+# finding indices of values 
+# using naive method  
+  
+# initializing list  
+test_list = [1, 3, 4, 3, 6, 7] 
+  
+# using naive method 
+# to find indices for 3 
+res_list = [] 
+for i in range(0, len(test_list)) : 
+    if test_list[i] == 3 : 
+        res_list.append(i) 
+        
+res_list
+
+# Python3 code to demonstrate  
+# finding indices of values 
+# using list comprehension  
+  
+# initializing list  
+test_list = [1, 3, 4, 3, 6, 7] 
+  
+# printing initial list  
+print ("Original list : " + str(test_list)) 
+  
+# using list comprehension 
+# to find indices for 3 
+res_list = [i for i in range(len(test_list)) if test_list[i] == 3] 
+          
+# printing resultant list  
+print ("New indices list : " + str(res_list)) 
+
+# Python3 code to demonstrate 
+# finding indices of values 
+# using enumerate() 
+
+# initializing list 
+test_list = [1, 3, 4, 3, 6, 7] 
+
+# printing initial list 
+print ("Original list : " + str(test_list)) 
+
+# using enumerate() 
+# to find indices for 3 
+res_list = [i for i, value in enumerate(test_list) if value == 3] 
+		
+# printing resultant list 
+print ("New indices list : " + str(res_list)) 
+
+
+# Python3 code to demonstrate 
+# finding indices of values 
+# using filter() 
+
+# initializing list 
+test_list = [1, 3, 4, 3, 6, 7] 
+
+# printing initial list 
+print ("Original list : " + str(test_list)) 
+
+# using filter() 
+# to find indices for 3 
+res_list = list(filter(lambda x: test_list[x] == 3, range(len(test_list)))) 
+		
+# printing resultant list 
+print ("New indices list : " + str(res_list)) 
